c9b537cd1ae41b7ff27cc594c5f852c82f914071
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-01-29 21:24:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import {useState} from 'react'
 function App() {
+  const [formData,setFormData] = useState({
+    username:"",
+    email:"",
+    phone:"",
+    dob:""
+  })
+  const makeFormVisible = ()=>{
+    let formDiv = document.getElementsByClassName("modal-content")[0]
+    formDiv.style.display = "flex"
+  }
+  const formHandle = (e)=>{
+    e.preventDefault()
+    if(e.target[2].value.length < 10){
+      alert("Invalid phone number. Please enter a 10-digit phone number.")
+    }
+    else if(new Date(e.target[3].value) > new Date()){
+      alert("Invalid date of birth. Date of birth cannot be in the future.")
+    }
+    else{setFormData({
+      username:"",
+      email:"",
+      phone:"",
+      dob:""
+    })}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="modal">
+      <h1>User Details Modal</h1>
+      <button className="buttonStyle" onClick={makeFormVisible}>Open Form</button>
+      <div className="modal-content">
+      <h1>Fill Details</h1>
+      <form className="formContainer" onSubmit={formHandle}>
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" className="inputFields" value={formData.username} onChange={(e)=>{setFormData(prevState => ({
+      ...prevState,
+      ['username']: e.target.value
+    }))}} required/>
+        <label htmlFor="email">Email Address:</label>
+        <input id="email" type="email" className="inputFields" value={formData.email} onChange={(e)=>{setFormData(prevState => ({
+      ...prevState,
+      ['email']: e.target.value
+    }))}} required/>
+        <label htmlFor="phone">Phone Number:</label>
+        <input id="phone" type="number" className="inputFields" value={formData.phone} onChange={(e)=>{setFormData(prevState => ({
+      ...prevState,
+      ['phone']: e.target.value
+    }))}} required/>
+        <label htmlFor="dateofbirth">Date of Birth:</label>
+        <input id="dateofbirth" type="date" className="inputFields" value={formData.dob} onChange={(e)=>{setFormData(prevState => ({
+      ...prevState,
+      ['dob']: e.target.value
+    }))}} required/>
+        <button className="buttonStyle" type="submit">Submit</button>
+      </form>
+      </div>
     </div>
   );
 }
